5cc940f33e801877f09f2c751752ae5c87001234
duchong <duchong@qianxin.com>
2022-09-29 10:41:22 +0800

    day0929

--- sql/leetcodelearn.sql
+++ sql/leetcodelearn.sql
@@ -24,3 +24,8 @@ order by user_id
 ###########################
 # 按日期分组销售产品
 
+
+select sell_date,count(distinct product) as num_sold ,group_concat(distinct product) as products
+from Activities
+group by sell_date
+order by sell_date asc
