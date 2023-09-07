e3b94b44e28449a68a0cce32ebb829b80b681575
duchong <duchong@qianxin.com>
2023-09-07 10:46:41 +0800

    test:append ...

--- test/2023/September/slice_test.go
+++ test/2023/September/slice_test.go
@@ -24,3 +24,32 @@ func TestSlice(t *testing.T) {
 		fmt.Println("s2 is not nil")
 	}
 }
+
+/*
+*
+
+	golang 中的 ... 有2种作用
+
+1. 作为函数的可变参数
+2. 将slice的元素打散传递
+*/
+func TestPointPointPoint(t *testing.T) {
+
+	s1 := []string{"hello", "world", "golang"}
+	s2 := [3]string{"hello", "world", "golang"}
+
+	fmt.Printf("%T\n", s1)
+	fmt.Printf("%T\n", s2)
+
+	Print(s1...)
+	fmt.Println("----------")
+	//Cannot use 's2' (type [3]string) as the type []string
+	//Print(s2...)
+
+}
+
+func Print(args ...string) {
+	for i, v := range args {
+		fmt.Printf("i = %v,v = %v\n", i, v)
+	}
+}
