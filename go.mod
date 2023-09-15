8dcdfb22907e3cc9838c4d31724e6d215595430b
duchong <duchong@qianxin.com>
2023-09-15 16:44:12 +0800

    test:casbin demo

--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module golang_demo/golang_study
 go 1.17
 
 require (
-	github.com/fogleman/pt v0.0.0-20170619012416-6fa0015c2178
+	github.com/casbin/casbin/v2 v2.77.2
 	github.com/robfig/cron/v3 v3.0.0
+)
+
+require (
+	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,4 +1,45 @@
 package main
 
+import (
+	"github.com/casbin/casbin/v2"
+	"log"
+)
+
 func main() {
+	a, err := casbin.NewEnforcer("./model.conf", "./policy.csv")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//a.AddFunction("my_func", KeyMatchFunc)
+	Check(a, "dajun", "data1", "read")
+	Check(a, "dajun", "data1", "write")
+	Check(a, "dajun", "data2", "read")
+	Check(a, "lizi", "data2", "write")
 }
+
+func Check(e *casbin.Enforcer, sub, obj, act string) {
+	ok, _ := e.Enforce(sub, obj, act)
+	if ok {
+		log.Printf("%s can %s %s \n", sub, act, obj)
+	} else {
+		log.Printf("%s can't %s %s \n", sub, act, obj)
+	}
+}
+
+//
+//func KeyMatch(key1 string, key2 string) bool {
+//	i := strings.Index(key2, "*")
+//	if i == -1 {
+//		return key1 == key2
+//	}
+//	if len(key1) > i {
+//		return key1[:i] == key2[:i]
+//	}
+//	return key1 == key2[:i]
+//}
+//
+//func KeyMatchFunc(args ...interface{}) (interface{}, error) {
+//	name1 := args[0].(string)
+//	name2 := args[1].(string)
+//	return (bool)(KeyMatch(name1, name2)), nil
+//}
